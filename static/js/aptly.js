8e5ba0d461cdb5d74d2230cf4ae55ab563f719b9
Andrey Smirnov <Smirnov.Andrey@gmail.com>
2014-08-17 22:09:30 +0400

    Redirect from old #hash links.

--- static/js/aptly.js
+++ static/js/aptly.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var highlight =     function() {
+  var highlight = function() {
        var lines = $(this).html().split("\n")
        for (i = 0; i < lines.length; i++) {
         if (lines[i].slice(0, 2) == "$ ") {
@@ -18,3 +18,52 @@ $(document).ready(function() {
   $("pre code").each(highlight)
   $("pre.code").each(highlight)
 })
+
+function redirectLegacyHashes() {
+    if (document.location.pathname != "/") {
+        return
+    }
+
+    var newpath = "", hash = document.location.hash
+
+    switch(hash) {
+    case "#examples":
+    case "#download":
+    case "#support":
+        newpath = "/" + hash.slice(1, hash.length) + "/"
+        break
+    case "#usage":
+        newpath = "/doc/overview/"
+        break
+    case "#common-options":
+        newpath = "/doc/aptly/flags/"
+        break
+    case "#package-query":
+        newpath = "/doc/feature/query/"
+        break
+    case "#duplicate-packages":
+        newpath = "/doc/feature/duplicate/"
+        break
+    case "#s3-publishing":
+        newpath = "/doc/feature/s3/"
+        break
+    case "#gpg-keys":
+        newpath = "/doc/aptly/publish/"
+        break
+    case "#commands":
+    case "#configuration":
+    case "#history":
+        newpath = "/doc/" + hash.slice(1, hash.length) + "/"
+        break
+    default:
+        if (/#aptly-/.test(hash)) {
+            newpath = "/doc/" + hash.slice(1, hash.length).replace(/-/g, "/") + "/"
+        }
+    }
+
+    if (newpath != "") {
+        document.location.replace(document.location.origin + newpath)
+    }
+}
+
+redirectLegacyHashes()
